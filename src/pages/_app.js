e08509c4dbcb7a80df51cebd815260dcac545275
Luan Meira Quevedo <luan.quevedo@hotmail.com>
2025-02-06 19:03:50 -0300

    atualizado dependencias

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,10 @@
 import MainLayout from '../layouts/MaindLayout';
+import '../styles/globals.scss'; // Mantenha isso para o estilo global
+import '../src/components/summary/index.scss';
+import '../src/components/skills/index.scss';
+import '../src/components/navbar/index.scss';
+import '../src/components/footer/index.scss';
+import '../components/cards/index.scss';
 
 
 function MyApp({ Component, pageProps }) {
