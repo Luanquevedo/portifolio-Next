0515fceff6fda8df28d381cc518779386f1be078
Luan Meira Quevedo <luan.quevedo@hotmail.com>
2025-02-06 19:21:08 -0300

    corrigido import dos arquivos scss

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
 import MainLayout from '../layouts/MaindLayout';
-
+import '../styles/globals.scss'; // Mantenha isso para o estilo global
+import '../components/summary/index.scss';
+import '../components/skills/index.scss';
+import '../components/navbar/index.scss';
+import '../components/footer/index.scss';
+import '../components/cards/index.scss';
 
 
 function MyApp({ Component, pageProps }) {
