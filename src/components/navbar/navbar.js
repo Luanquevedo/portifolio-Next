155a5164e47cdd84749c7cb0fd94d0a4ccb6ae44
Luan Meira Quevedo <luan.quevedo@hotmail.com>
2025-02-14 20:48:51 -0300

    Aprimorado responsividade da seção de apresentação para desktop

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import { Link } from 'react-scroll';
 
+//links do navbar a serem renderizados.
+const linkItems = [
+  { to: "summary", label: "Apresentação"},
+  { to: "projects", label: "Projetos"},
+  { to: "skills", label: "Habilidades"},
+  { to: "contact", label: "Contato"},
+];
+
 const Navbar = () => {
   return (
     <nav className='navbar'>
       <img src='images/logo.svg' alt="Logo" />
       <div className='navbar__Itens'>
-        <Link to="summary" smooth={true} duration={500}>Apresentação</Link>
-        <Link to="projects" smooth={true} duration={500}>Projetos</Link>
-        <Link to="skills" smooth={true} duration={500}>Habilidades</Link>
-        <Link to="contact" smooth={true} duration={500}>Contato</Link>
+        {linkItems.map((item) => (
+          <Link key={item.to} to={item.to} smooth={true} duration={500}>
+            {item.label}
+          </Link> 
+        ))}
       </div>
     </nav>
   );
