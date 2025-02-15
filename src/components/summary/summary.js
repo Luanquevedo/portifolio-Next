e7736822a55ed8bd8c395526f094697f5168e239
Luan Meira Quevedo <luan.quevedo@hotmail.com>
2025-02-14 21:09:35 -0300

    Otimizado responsividade e semantica  da seção de apresentação

--- src/components/summary/summary.js
+++ src/components/summary/summary.js
@@ -1,22 +1,23 @@
-
+import React from "react";
 
 const Summary = () => {
     return (
-        <article className='summary'>
+        <article className='summary' aria-labelledby="summary-title">
             <div className='summary__Content'>
-                <h2>Bem vindo ao meu Portifolio</h2>
+                <h2 id="summary-title">Bem vindo ao meu Portifolio</h2>
                 <h1>Olá! Sou Luan Meira Quevedo
                     Sou um desenvolvedor web </h1>
-                <p className="summary__Content__Text">Desenvolvedor web com experiência em React,
-                    Next.js, Node.js, MongoDB e PostgreSQL,
+                <p className="summary__Content__Text">Desenvolvedor web com experiência em <strong>React,
+                    Next.js, Node.js, MongoDB e PostgreSQL</strong>,
                     focado em soluções escaláveis e de alta qualidade.
                 </p>
-                <div className='summary__Content__Button'>
+
+                <nav className='summary__Content__Button' aria-labelledby="Links para mais informações">
                     <button className='summary__Button'>Projetos</button>
                     <button className='summary__Button' onClick={() => window.open('https://www.linkedin.com/in/luan-quevedo/', "_blank")}>Conectar-se</button>
-                </div>
+                </nav>
             </div>
-            <img src='images/photo.svg' />
+                <img src='images/photo.svg' alt="Foto Luan Quevedo" />
         </article>
     )
 }
