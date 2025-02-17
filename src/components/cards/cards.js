93c1e786f628eec834ef5189a964514754dd1089
Luan Meira Quevedo <luan.quevedo@hotmail.com>
2025-02-17 19:13:01 -0300

    Realizado a criação de um componente card na qual possui autoplay e slide com suporte mobile e renderização de detalhes atraves de map.  Lib adicionada swiper

--- src/components/cards/cards.js
+++ src/components/cards/cards.js
@@ -1,43 +1,20 @@
-'use client';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 
-import { useState, useEffect } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-import { ChevronLeft, ChevronRight } from 'lucide-react';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { color } from "framer-motion";
 
 const projetos = [
-  { id: 1, title: 'Partnership', tecnology: "tecnologias", description: 'Descrição do card 1', image: '/images/plugow.svg' },
-  { id: 2, title: 'Brazilian Hands', tecnology: "tecnologias", description: 'Descrição do card 2', image: '/images/brazilian.svg' },
-  { id: 3, title: 'Morais & Ribeiro', tecnology: "tecnologias", description: 'Descrição do card 3', image: '/images/morais.svg' },
-  { id: 4, title: 'Innovar', tecnology: "tecnologias", description: 'Descrição do card 4', image: '/images/innovar.svg' },
+  { id: 1, title: 'Partnership', tecnology: "Tecnologias Utilizadas: Next.js, React.js, Html, Tailwind css, Typescript", description: 'Projeto realizado com intuito de captar candidatos paraplataforma de beneficios partnership', link: 'https://swiperjs.com/swiper-api#modules', image: '/images/plugow.svg' },
+  { id: 2, title: 'Brazilian Hands', tecnology: "Tecnologias Utilizadas: React.js, Sass, Html, Javascript, MongoDB, Node.js, Express.js", description: 'Projeto realizado para captação de clietes e colaboradores para serviçoes domiciliares prestados na irlanda com prestadores brasileiros', link: 'https://swiperjs.com/swiper-api#modules', image: '/images/brazilian.svg' },
+  { id: 3, title: 'Morais & Ribeiro', tecnology: "Tecnologias Utilizadas: Next.js, React.js, Sass, Html, Javascript, Bootstrap", description: 'Projeto para divulgação do escritorio de advocatica e captação de possiveis clientes', link: 'https://swiperjs.com/swiper-api#modules', image: '/images/morais.svg' },
+  { id: 4, title: 'Innovar', tecnology: "Tecnologias Utilizadas: React.js, Sass, Html, Javascript", description: 'Projeto de captação para potenciais clientes para a empresa', link: 'https://swiperjs.com/swiper-api#modules', image: '/images/innovar.svg' },
 ];
 
 export default function Carousel() {
-  const [index, setIndex] = useState(0);
-  const [imagesLoaded, setImagesLoaded] = useState(false);
-
-  // Função para garantir que todas as imagens estão carregadas
-  const handleImageLoad = () => {
-    setImagesLoaded(true);
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prevIndex) => (prevIndex + 1) % projetos.length);
-    }, 5000); // 10 segundos para mudança de slide
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const nextSlide = () => setIndex((prev) => (prev + 1) % projetos.length);
-  const prevSlide = () => setIndex((prev) => (prev - 1 + projetos.length) % projetos.length);
-
-  // Função para pegar os três projetos atuais do carrossel
-  const currentProjects = [
-    projetos[(index) % projetos.length],
-    projetos[(index + 1) % projetos.length],
-    projetos[(index + 2) % projetos.length],
-  ];
-
   return (
     <div className="container">
       <div className='container__Text'>
@@ -45,35 +22,37 @@ export default function Carousel() {
         <p>|</p>
         <a>Visualizar mais</a>
       </div>
+      <Swiper
+        slidesPerView={2}
+        modules={[Pagination, Autoplay]}
+        pagination
+        autoplay={{ delay: 3000, disableOnInteraction: true }}
+        speed={700}>
+        {projetos.map((item) => (
+          <SwiperSlide key={item.id} >
+            <div className="container__Carrousel">
+              <h3>{item.title}</h3>
+              <div className="container__Carrousel__Content">
+                <div className="container__Carrousel__Content__Img">
+                  <img src={item.image}
+                    alt={item.title}
+                    className='slide__item' />
+                </div>
+                <div className="container__Carrousel__Content__Text">
+                  <h3 >{item.tecnology}</h3>
+                  <p>{item.description}</p>
+                  <a href={item.link} target="_blank" rel="noopener noreferrer">Ver mais</a>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+
+      </Swiper>
       <div className="container__Carousel">
-        <AnimatePresence mode="popLayout">
-          {currentProjects.map((projeto) => (
-            <motion.div
-              key={projeto.id}
-              initial={{ opacity: 0, x: 50 }}
-              animate={{ opacity: imagesLoaded ? 1 : 0, x: 0 }}
-              exit={{ opacity: 0, x: -50 }}
-              transition={{ duration: 0.5 }}
-              className={`carousel-card ${imagesLoaded ? 'loaded' : 'loading'}`} // Adiciona a classe com base no estado
-            >
-              <img
-                src={projeto.image}
-                alt={projeto.title}
-                className="carousel-image"
-                onLoad={handleImageLoad} // Espera o carregamento da imagem
-              />
-            </motion.div>
-          ))}
-        </AnimatePresence>
       </div>
 
-      {/* Botões de navegação */}
-      <button onClick={prevSlide} className="carousel-btn prev-btn">
-        <ChevronLeft size={24} className="text-white" />
-      </button>
-      <button onClick={nextSlide} className="carousel-btn next-btn">
-        <ChevronRight size={24} className="text-white" />
-      </button>
+
     </div>
   );
 }
